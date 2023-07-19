28749e942e95619df39d8a311e39ca137b279000
phanduc <phanminhduc159@gmail.com>
2023-07-19 17:59:35 +0700

    first commit

---
+++ eProject3_6/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using eProject3_6.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace eProject3_6.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult contact() 
+        { 
+            return View();
+        }
+        public IActionResult loginandsignup() 
+        {
+            return View();
+        }
+        public IActionResult aboutus() 
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ eProject3_6/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace eProject3_6.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
